4208a1767d69a51ffe7e3b74a4b3bb9f0c798ac8
Sannoob <cotwo0139@gmail.com>
2021-01-03 09:10:31 +0900

    added config.js

---
+++ config.inc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tcpListenIp: '0.0.0.0', // TCP Server Listening IP (I don't recommend changing this)
+  hostIp: '192.168.191.153', // Server Host IP (runs this app)
+  broadCastIp: '192.168.191.255', // Host IP's broadcast address (example: 192.168.100.101 -> 192.168.100.255)
+  broadCastInterval: 1000 // Broadcast interval (ms) 1sec = 1000ms
+}
